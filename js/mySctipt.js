a5b5f8c18c2ce1436699842cd4627c6b9e22b31f
sazeduzzaman <sazeduzzaman@yahoo.com>
2022-07-12 01:08:49 +0600

    done

---
+++ js/mySctipt.js
@@ -0,0 +1,35 @@
+//Refresh button 
+function refresh() {
+  window.location.reload("Refresh")
+}
+
+//𝗗𝗶𝗴𝗶𝘁𝗮𝗹 𝗧𝗶𝗺𝗲 𝗦𝗵𝗼𝘄𝗶𝗻𝗴
+function showTime(){
+  var date = new Date();
+  var h = date.getHours(); // 0 - 23
+  var m = date.getMinutes(); // 0 - 59
+  var s = date.getSeconds(); // 0 - 59
+  var session = "AM";
+  
+  if(h == 0){
+      h = 12;
+  }
+  
+  if(h > 12){
+      h = h - 12;
+      session = "PM";
+  }
+  
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  s = (s < 10) ? "0" + s : s;
+  
+  var time = h + ":" + m + ":" + s + " " + session;
+  document.getElementById("time").innerText = time;
+  document.getElementById("time").textContent = time;
+  
+  setTimeout(showTime, 1000);
+  
+}
+
+showTime();
